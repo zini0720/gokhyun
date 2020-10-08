2adcb05c92f8e0823bf7eb51ebdd61c10ef222e4
HwijinKwon <zini072@gmail.com>
2020-10-08 11:41:06 +0900

    just testing while assignment

---
+++ assignment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void git(int n);
+
+int main(){
+	
+	printf("Hello git!\n");
+	
+	return 0;
+}
+
+void git(int n){
+	if(n){
+		printf("I hate git\n");
+	}
+	else printf("I love git\n");
+}
