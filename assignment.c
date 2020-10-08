ee768a12627d9d911b4f4cd43e673aad7c1eeca4
HwijinKwon <zini072@gmail.com>
2020-10-08 11:58:41 +0900

    upload testfile for assignment

---
+++ assignment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void git(int n);
+
+int main(){
+	
+	printf("Hello git!\n");
+	
+	printf("Hello git from br1\n");
+
+	return 0;
+}
+
+void git(int n){
+	if(n){
+		printf("I hate git\n");
+	}
+	else printf("I love git\n");
+}
